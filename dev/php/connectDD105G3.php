21aff9d9c6747e9aea1f986af9006117b8231a11
syy717 <imvikki717@gmail.com>
2020-03-31 22:40:40 +0800

    change phpmyadmin connect for local server testing

--- dev/php/connectDD105G3.php
+++ dev/php/connectDD105G3.php
@@ -2,7 +2,7 @@
 	$dname = 'dd105g3';
 	$dsn = "mysql:host=localhost;port=3306;dbname=$dname;charset=utf8";
 	$user = "root";
-	$password = "a123456";
+	$password = "1234";
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
 	$pdo = new PDO( $dsn, $user, $password, $options);
 ?>
